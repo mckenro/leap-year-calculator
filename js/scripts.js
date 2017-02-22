1b2d095bf4fcf88f67aad3f86b647ceed3e51f06
Max Larson and Rob McKenzie <rob@>
2017-02-22 09:11:18 -0800

    add initial html input and function for algorithmically determining years divisible by 4

---
+++ js/scripts.js
@@ -0,0 +1,36 @@
+//Backend
+var leapYear = function(inputYear) {
+if (inputYear % 4 === 0) {
+  return true;
+}else {
+  return false;
+}
+};
+
+$(document).ready(function() {
+  $("form#userInput").submit(function(event){
+    event.preventDefault();
+    var inputYear = parseInt($("input#year").val());
+    var result = leapYear(inputYear);
+    console.log(result);
+  });
+});
+
+
+
+
+// The program should return when a year is not a leap year.
+// Input Example: 1993
+// Output Example: false
+
+
+
+// The program should return 'true' for years divisible by 4, since those are leap years.
+// Input Example: 2004
+// Output Example: true
+// The program should return 'false' for years divisible by 100, since those are not leap years.
+// Input Example: 1900
+// Output Example: false
+// The program should return 'true' for years divisible by 400, since those are leap years.
+// Input Example: 2000
+// Output Example: true
