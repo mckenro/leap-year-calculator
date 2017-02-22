8143ced420e5b11d91315a7c0139466cf2bbe5a1
Max Larson and Rob McKenzie <rob@>
2017-02-22 09:45:45 -0800

    change hit dis to get result on submit button

--- js/scripts.js
+++ js/scripts.js
@@ -7,31 +7,13 @@ if ((inputYear % 4 === 0) && (inputYear % 100 !== 0) || (inputYear % 400 === 0))
 }
 };
 
+//Frontend
 $(document).ready(function() {
   $("form#userInput").submit(function(event){
     event.preventDefault();
-    var inputYear = parseInt($("input#year").val());
-    var result = leapYear(inputYear);
+    var userYear = parseInt($("input#year").val());
+    var result = leapYPigear(userYear);
     $("#result").text(result);
     //console.log(result);
   });
 });
-
-
-
-
-// The program should return when a year is not a leap year.
-// Input Example: 1993
-// Output Example: false
-
-
-
-// The program should return 'true' for years divisible by 4, since those are leap years.
-// Input Example: 2004
-// Output Example: true
-// The program should return 'false' for years divisible by 100, since those are not leap years.
-// Input Example: 1900
-// Output Example: false
-// The program should return 'true' for years divisible by 400, since those are leap years.
-// Input Example: 2000
-// Output Example: true
